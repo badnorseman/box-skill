935497a13a736af212de8237046507034822b987
Dan Jensen <urbanvikingr@gmail.com>
2019-04-16 22:19:13 +0200

    add new methods on model class

--- Model.js
+++ Model.js
@@ -1,99 +1,92 @@
 "use strict";
 const FormData = require("form-data");
 const fs = require("fs");
-const https = require("http");
 const request = require("request");
 
 class Model {
-  constructor(imageURL) {
-    this.imageURL = imageURL;
+  constructor() {
     this.modelURL =
       process.env.MODEL_URL || "http://138.197.66.204:5000/modelpost";
   }
 
   /**
-   * Model.get
-   * @param {string} imageURL  url to image
-   * @return {Object}          data from model
+   * Model.readFile
+   * @param {string} fileURL  url to file
+   * @return {Object}         data from model
    */
-  get() {
+  readFile(fileURL) {
     return new Promise((resolve, reject) => {
-      const req = https.request(this.modelURL, res => {
-        if (res.statusCode !== 200) {
-          const error = new Error(res.statusCode);
-          reject(`Model returned error on call ${error}`);
-        }
-
-        let rawData = "";
-        res.setEncoding("utf8");
-        res.on("data", chunk => (rawData += chunk));
-        console.debug(`rawData: ${rawData}`); // Remove when implemented successfully
-        res.on("end", () => {
-          try {
-            const parsedData = JSON.parse(rawData);
-            resolve(parsedData);
-          } catch (error) {
-            reject(`Model returned error on parse ${error}`);
-          }
-        });
-      });
-      req.on("error", error => {
+      const file = __dirname + fileURL;
+      const stream = fs.createReadStream(file);
+      console.debug(`file: ${file}\nstream: ${JSON.stringify(stream)}`); // Remove when implemented successfully
+      let data;
+      stream.on("error", error => {
         reject(`Model returned error on call ${error}`);
       });
-      req.end();
-    });
-  }
-
-  /**
-   * Model.getImage
-   * @param {string} imageURL  url to image
-   * @return {Object}          data from model
-   */
-  getImage() {
-    return new Promise((resolve, reject) => {
-      request(this.imageURL)
-        .on("error", error => {
-          reject(`Model returned error on call ${error}`);
-        })
-        .on("response", res => {
-          console.debug(`statusCode: ${res.statusCode}`); // Remove when implemented successfully
-          console.debug(`res: ${JSON.stringify(res)}`); // Remove when implemented successfully
-          resolve({
-            body: JSON.stringify({
-              message: "Processed successfully"
-            })
-          });
+      stream.on("data", chunk => {
+        data += chunk;
+        // process.stdout.write(chunk);
+      });
+      stream.on("end", () => {
+        console.debug(`length: ${data.length}`); // Remove when implemented successfully
+        resolve({
+          body: JSON.stringify({
+            message: "Processed successfully"
+          })
         });
+      });
     });
   }
 
   /**
-   * Model.post
-   * @param {string} imageURL  url to image
-   * @return {Object}          data from model
+   * Model.testupload
+   * @param {string} fileURL  url to file
+   * @return {Object}         data from model
    */
-  post() {
+  testupload(fileURL) {
     console.debug(`modelURL: ${this.modelURL}`); // Remove when implemented successfully
 
     return new Promise((resolve, reject) => {
-      // request(this.imageURL).pipe(fs.createWriteStream("newImage.jpg"));
-
       const formData = new FormData();
-      // formData.append("data", request(this.imageURL));
-      formData.append("file", fs.createReadStream("./image.jpg"));
-      console.debug(`formData: ${JSON.stringify(formData)}`); // Remove when implemented successfully
+      formData.append("file", request(fileURL));
 
       const options = {
-        url: this.modelURL,
-        headers: {
-          "Content-Type": "image/jpeg",
-          "Content-Length": Buffer.byteLength(this.imageURL)
-        }
+        hostname: this.hostname,
+        form: formData
       };
       console.debug(`options: ${JSON.stringify(options)}`); // Remove when implemented successfully
 
-      request.post(options, formData, (error, res, body) => {
-        console.debug(`res: ${res}`); // Remove when implemented successfully
+      request.post(options, (error, res, body) => {
+        console.debug(`statusCode: ${JSON.stringify(res.statusCode)}`); // Remove when implemented successfully
+        console.debug(`body: ${JSON.stringify(body)}`); // Remove when implemented successfully
+        if (error) {
+          reject(`Model returned error on call ${error}`);
+        }
+        resolve({
+          body: JSON.stringify({
+            message: "Processed successfully"
+          })
+        });
+      });
+    });
+  }
+
+  /**
+   * Model.uploadfile
+   * @param {string} fileURL  url to file
+   * @return {Object}         data from model
+   */
+  uploadfile(fileURL) {
+    return new Promise((resolve, reject) => {
+      const formData = new FormData();
+      formData.append("file", request(fileURL));
+
+      const options = {
+        uri: this.modelURL,
+        form: formData
+      };
+
+      request.post(options, (error, res, body) => {
         if (error) {
           reject(`Model returned error on call ${error}`);
         }
--- _tests_/Model.test.js
+++ _tests_/Model.test.js
@@ -2,25 +2,25 @@ const Model = require("../Model");
 
 describe("Model", () => {
   test.skip("gives data when processing succeeds", async () => {
-    const imageURL = "https://picsum.photos/4434/3729?image=22";
-    const m = new Model(imageURL);
-    const data = await m.get();
-    expect(JSON.parse(data.body)).toEqual({
-      message: "Processed successfully"
-    });
-  });
-  test("gives data when processing succeeds", async () => {
-    const imageURL = "https://picsum.photos/4434/3729?image=22";
-    const m = new Model(imageURL);
-    const data = await m.getImage();
+    const m = new Model();
+    const fileURL = "https://picsum.photos/4434/3729?image=22";
+    const data = await m.testupload(fileURL);
     expect(JSON.parse(data.body)).toEqual({
       message: "Processed successfully"
     });
   });
   test.skip("gives data when processing succeeds", async () => {
-    const imageURL = "https://picsum.photos/4434/3729?image=22";
-    const m = new Model(imageURL);
-    const data = await m.post();
+    const m = new Model();
+    const fileURL = "https://picsum.photos/4434/3729?image=22";
+    const data = await m.uploadfile(fileURL);
+    expect(JSON.parse(data.body)).toEqual({
+      message: "Processed successfully"
+    });
+  });
+  test("gives data when processing succeeds", async () => {
+    const m = new Model();
+    const fileURL = "/images/coolguy.jpg";
+    const data = await m.readFile(fileURL);
     expect(JSON.parse(data.body)).toEqual({
       message: "Processed successfully"
     });
